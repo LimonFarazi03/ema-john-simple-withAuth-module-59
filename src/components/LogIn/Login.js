4a370498429b1722694697b14224e9bc225d903b
MD Limon Farazi <abidislamabir@gmail.com>
2022-04-20 00:25:55 +0600

    some impliments

--- src/components/LogIn/Login.js
+++ src/components/LogIn/Login.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link } from "react-router-dom";
 import Glogo from '../../images/g-logo.png'
 
 const Login = () => {
+
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [error,setError] = useState('')
+
+  const handleEmailBlur = (event) =>{
+    setEmail(event.target.value);
+    console.log(email)
+  }
+  const handlePasswordBlur = (event) =>{
+    setPassword(event.target.value);
+    console.log(password)
+  }
+  // On Submit
+  const handleCreateUser = (event) =>{
+    event.preventDefault();
+  }
   return (
     <div className="form-container">
       <div>
         <h1 className="form-title">Login</h1>
-        <form action="">
+        <form onSubmit={handleCreateUser}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+            <input onBlur={handleEmailBlur} type="email" name="email" required/>
           </div>
           <div className="input-group">
             <label htmlFor="email">Password</label>
-            <input type="password" name="password" />
+            <input onBlur={handlePasswordBlur} type="password" name="password" required/>
           </div>
-          <input className="form-submit-btn" type="submit" />
+          <input className="form-submit-btn" type="submit" value='Log In'/>
         </form>
         <p className="signInOut">New to Ema-john? <Link className="form-link" to='/signup'>Create New Account</Link></p>
         <div className="connect-google">
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Signup.css';
 import Glogo from '../../images/g-logo.png'
 
 const Signup = () => {
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [confirmPassword,setConfirmPassword] = useState('')
+  const [error,setError] = useState('')
+
+  const handleEmailBlur = (event) =>{
+    setEmail(event.target.value);
+    console.log(email)
+  }
+  const handlePasswordBlur = (event) =>{
+    setPassword(event.target.value);
+    console.log(password)
+  }
+  const handleConfirmPasswordBlur = (event) =>{
+    setConfirmPassword(event.target.value);
+    console.log(confirmPassword)
+  }
+  // On Submit
+  const handleCreateUser = (event) =>{
+    event.preventDefault();
+    if(password !== confirmPassword){
+      setError("Your password did't same")
+      return
+    }
+  }
+
   return (
     <div className="form-container">
       <div>
         <h1 className="form-title">SignUp</h1>
-        <form action="">
+        <form onSubmit={handleCreateUser}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" />
+            <input onBlur={handleEmailBlur} type="email" name="email" required/>
           </div>
           <div className="input-group">
             <label htmlFor="email">Password</label>
-            <input type="password" name="password" />
+            <input onBlur={handlePasswordBlur} type="password" name="password" required/>
           </div>
           <div className="input-group">
-            <label htmlFor="email">Confirm Password</label>
-            <input type="password" name="password" />
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" required/>
           </div>
-          <input className="form-submit-btn" type="submit" />
+          <input className="form-submit-btn" type="submit" value='Sign Up' />
         </form>
+        <p style={{color:'red'}}>{error}</p>
         <p className="signInOut">Already have an account? <Link className="form-link" to='/login'>Login</Link></p>
         <div className="connect-google">
           <img src={Glogo} alt="Google-logo"/>
